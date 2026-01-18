f26add086ae575eebbedc8b6078de1b7bda58f44
Hendrik Jansen <hendrikjansen@posteo.net>
2026-01-18 17:17:29 +0100

    added dictionary and continued code

--- strands_solver.py
+++ strands_solver.py
@@ -1,24 +1,34 @@
 from nltk.corpus import words
 
-class path():
-    def __init__(self, coord_list):
-        self.coord_list = coord_list
+DICT = []
 
-    def is_word():
+with open('words.txt', 'r') as file:
+    for line in file:
+        DICT.append(line[:-2].lower())
+
+print(DICT)
+
+class path():
+    def __init__(self, coord_list, field):
+        self.coord_list = coord_list
+        self.field = field
+        word_pointer_start = None
+        word_pointer_end = None      
+
+    def is_word(self):
         pass
+
+    def is_prefix(self):
+        pass
+
+    def __repr__(self):
+        ''.join([self.field[i][j] for i, j in self.coord_list])
+
 
 
 class puzzle():
     def __init__(self, field):
         field = field
-
-    def path_is_word(self, path):
-        #TODO
-        pass
-
-    def path_is_prefix(self, path):
-        #TODO
-        pass
 
     def find_words_from(self, coords):
         words = []
@@ -30,10 +40,8 @@ class puzzle():
                 if path.is_word():
                     words.append(path.string())
                 if not path.is_prefix():
+                    pass
 
-
-
-words = []
 
 for i in range(len(puzzle)):
     for j in range(len(puzzle[i])):
